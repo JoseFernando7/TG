37919c9b6ca7bcd7538333a7177803c53478c191
JoseFernando7 <jose.f72000@gmail.com>
2024-10-16 23:41:01 -0500

    Feat(Check collisions): Check functionality of the hydrogen isotope collisioning with the reactor added, now I can add the reaction functionality

--- Assets/Scripts/Drag Functionality.cs
+++ Assets/Scripts/Drag Functionality.cs
@@ -1,39 +1,57 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragFunctionality : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DragFunctionality : MonoBehaviour
 {
+    private bool isDragging = false;
+    private Vector3 offset;
     private Vector3 originalPosition;
-    private bool isOverReactor = false;
 
-    void Start()
+    private void Start()
     {
+        // Save the original position of the object
         originalPosition = transform.position;
     }
 
-    public void OnBeginDrag(PointerEventData eventData)
+    private void OnMouseDown()
     {
-        // Empieza a arrastrar
+        // Check when the user clicks on the object
+        isDragging = true;
+        offset = transform.position - GetMouseWorldPos();
     }
 
-    public void OnDrag(PointerEventData eventData)
+    private void OnMouseUp()
     {
-        // Mientras arrastras, sigue la posición del mouse
-        transform.position = Input.mousePosition;
+        // Check when the user releases the object
+        isDragging = false;
+
+        // Disable temporarily the collider to avoid detecting the object itself
+        GetComponent<Collider2D>().enabled = false;
+
+        // Return the object to its original position
+        transform.position = originalPosition;
+
+        // Enable the collider again
+        GetComponent<Collider2D>().enabled = true;
     }
 
-    public void OnEndDrag(PointerEventData eventData)
+    private void Update()
     {
-        // Al soltar, si no está sobre el reactor, vuelve a la posición original
-        if (!isOverReactor)
+        // Check if the object is being dragged and update its position
+        if (isDragging)
         {
-            transform.position = originalPosition;
+            transform.position = GetMouseWorldPos() + offset;
         }
     }
 
-    // Llamado cuando entra en el "Reactor"
-    public void SetOverReactor(bool isOver)
+    // Get the mouse position in the 2D world
+    Vector3 GetMouseWorldPos()
     {
-        isOverReactor = isOver;
+        Vector3 mousePoint = Input.mousePosition;
+
+        // Keep the z coordinate of the mouse position
+        mousePoint.z = Camera.main.WorldToScreenPoint(transform.position).z;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 }
--- Assets/Scripts/Reactor.cs
+++ Assets/Scripts/Reactor.cs
@@ -1,19 +1,90 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Reactor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class Reactor : MonoBehaviour
 {
-    public DragFunctionality dragImage;
-
-    public void OnPointerEnter(PointerEventData eventData)
+    private enum Isotopes
     {
-        // Detecta cuando la imagen está sobre el "reactor"
-        dragImage.SetOverReactor(true);
+        None,
+        Hydrogen,
+    };
+    private Isotopes isotope = Isotopes.None;
+
+    public GameObject dragObject;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (IsDraggingHydrogen())
+            {
+                isotope = Isotopes.Hydrogen;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            // Disable temporarily the collider to avoid detecting the object itself
+            dragObject.GetComponent<Collider2D>().enabled = false;
+
+            CheckHidrogenCollision();
+
+            // Enable the collider again
+            dragObject.GetComponent<Collider2D>().enabled = true;
+        }
+
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    public bool IsDraggingHydrogen()
     {
-        // Detecta cuando la imagen sale del "reactor"
-        dragImage.SetOverReactor(false);
+        Vector2 mousePosition = GetMouseWorldPos();
+
+        // Throw a raycast from the mouse position
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+        if (hit.collider != null && hit.collider.CompareTag("Hydrogen"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void CheckHidrogenCollision()
+    {
+        Vector2 mousePosition = GetMouseWorldPos();
+
+        // Throw a raycast from the mouse position
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+        if (hit.collider != null && hit.collider.CompareTag("Reactor"))
+        {
+            // Debug.Log("The object is over a reactor");
+            if (isotope == Isotopes.Hydrogen)
+            {
+                Debug.Log("Hydrogen isotope placed in the reactor");
+                // TODO: Add the hydrogen isotope reaction functionality
+                isotope = Isotopes.None;
+            }
+        }
+        else
+        {
+            Debug.Log("The object is not over a reactor");
+            isotope = Isotopes.None;
+        }
+    }
+
+    // Get the mouse position in the 2D world
+    Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        // Keep the z coordinate of the mouse position
+        mousePoint.z = Camera.main.WorldToScreenPoint(transform.position).z;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 }
